7a7eeff826d183fb15db1a7dc3b4ffee86e31850
Marcus Vinicius Silva Francisquini <francisquini@academicos.com>
2025-04-07 21:19:26 -0300

    atividade feita

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // tipagem para a piada
+  type Joke = {
+    value: string;
+  };
+
+  // useState para a piada
+  const [joke, setJoke] = useState<string>("");
+
+  // useState para a imagem
+  const [img, setImg] = useState<string>("/angry.jpg");
+
+  // useState para o texto do botão
+  const [buttonText, setButtonText] = useState<string>(
+    "Gerar uma piada hilária"
+  );
+
+  // fetch API
+  const fetchJoke = async () => {
+    try {
+      const response = await fetch("https://api.chucknorris.io/jokes/random");
+
+      if (response.ok) {
+        const data: Joke = await response.json();
+        // Atualiza o estado com a piada
+        setJoke(data.value);
+        // Alterando a imagem ao chamar a api
+        setImg("/boobies.gif");
+        // Alterando o texto do botão ao chamar a api
+        setButtonText("Mais uma piada hilária?");
+      }
+    } catch (error) {
+      console.error("Erro ao buscar dados da API:", error);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <header>
+        <img src="/logo.png" className="logo-header" />
+      </header>
+      <main>
+        <img src={img} className="img-main" />
+        <button onClick={fetchJoke} className="fetch-btn">
+          {buttonText}
         </button>
+        <p id="joke-text">{joke}</p>
+      </main>
+      <footer>
+        <img src="/footer-gif.gif" />
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Criado por <span>Marcus Vinicius</span>, aluno na uniesp
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
